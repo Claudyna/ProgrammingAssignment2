17fe913764f6916569d2faf63c1b0f32cc1aca0e
Claudyna <33224558+Claudyna@users.noreply.github.com>
2017-11-06 23:23:43 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -10,12 +10,26 @@ set <- function(y) {                ## definiton of the set function to assign n
 x <<- y                             ## value of matrix in parent environment
 inv <<- NULL                        ## if matrix is new, reset inv to NULL
         }
+ get <- function() x                     ## definiton of getting function - returns value of the matrix argument
+     
+     setinverse <- function(inverse) inv <<- inverse  ## assignment value of inv in the parent environment
+    getinverse <- function() inv                     ## getting the value of inv where is called
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
+                                                                                 
+
+        }
         
-}
-
-
 ## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+     inv <- x$getinverse()
+     if(!is.null(inv)) {
+         message("getting cached matrix")
+         return(inv)
+     }
+     data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
+  }		  
